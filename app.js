c197aed2d23da6805f7c3421489a792f698c2ad5
kodaikodai <k.sagawaa@gmail.com>
2020-04-04 21:26:16 +0900

    update

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-
-  let task = document.querySelector("#task");
+//----------------------- 入力されたタスクを追加する↓↓ ----------------------------
+let task = document.querySelector("#task");
 let addList = document.querySelector("ul");
 let form = document.querySelector(".task_form");
 
-
 let buildHTML = text => {
   let html = `<li class = "list">
-                <span>${text}</span>
+                <span id = "task_text">${text}</span>
                 <i class="far fa-trash-alt delete"></i>
               </li>`;
   addList.innerHTML += html;
@@ -15,29 +14,48 @@ let buildHTML = text => {
 
 task.addEventListener("keydown", function(e) {
   let input = task.value;
-  
+
   if (e.which === 13) {
     // 押されたキーがエンターキーの時
-    if(input !== ""){
-    e.preventDefault();
-    // デフォルトのイベントを無効にする
-    buildHTML(input);
-    // addList.insertAdjacentHTML ('beforeend',buildHTML(input));
-    form.reset();
-    // フォームに入力した文字をクリアする
-    // reset()はフォームに対してセットする
-    }
+    if (input !== "") {
+      e.preventDefault();
+      // デフォルトのイベントを無効にする
+      buildHTML(input);
+      // addList.insertAdjacentHTML ('beforeend',buildHTML(input));
+      form.reset();
+      // フォームに入力した文字をクリアする
+      // reset()はフォームに対してセットする
+    }else{
+      e.preventDefault();
+      addList.insertAdjacentHTML ('beforeend','');
+      form.reset();
+      alert('タスクを入力してください');
+    };
   }
- 
 });
 
+// ------------------------ ゴミ箱マークをクリックしたらタスクが削除される↓↓----------------------------
 let btn = document.querySelector(".delete");
 
-addList.addEventListener("click",function(e){
-  if(e.target.classList.contains('delete')){
+addList.addEventListener("click", function(e) {
+  if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
 });
 
+//------------------- インクリメンタルサーチ↓↓-----------------------------
+let serch = document.querySelector('.serch_form');
+let keyword = document.querySelector('#keyword');
+let list = document.querySelector('list');
+let taskText = document.querySelector('#task_text')
 
-
+serch.addEventListener('keyup',function(){
+  let inputWord = keyword.value;
+  // addList.innerHTML='';
+  console.log(list);
+  // for(var i = 0;list.length; i += 1){
+  //   console.log(list[0].textContent);
+  // }
+  
+  // addList.textContent.forEach(element => console.log(element));
+});
